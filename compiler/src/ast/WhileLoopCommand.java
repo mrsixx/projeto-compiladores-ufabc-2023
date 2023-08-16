00a9470b424067c72d38c5202a3117e04b95382d
Emanoel Milagres <emanoel.milagres@gmail.com>
2023-08-16 00:06:38 -0300

    adicionado cmdif e cmdenquanto

--- compiler/src/ast/WhileLoopCommand.java
+++ compiler/src/ast/WhileLoopCommand.java
@@ -1,6 +1,15 @@
 package ast;
 
+import java.util.ArrayList;
+
 public class WhileLoopCommand extends Command {
+	private String condition;
+	private ArrayList<Command> lista;
+	
+	public WhileLoopCommand(String condition, ArrayList<Command> comandos) {
+		this.condition = condition;
+		this.lista = comandos;
+	}
 
 	@Override
 	public String cCompile() {
@@ -8,10 +17,23 @@ public class WhileLoopCommand extends Command {
 		return "";
 	}
 
+
+
 	@Override
 	public String javaCompile() {
-		// TODO Auto-generated method stub
-		return "";
-	}
+        StringBuilder str = new StringBuilder();
+		str.append("while ("+condition+") {\n");
+		for (Command cmd: lista) {
+			str.append("\t\t\t"+cmd.javaCompile());
+		}
+		str.append("\n}\n");
+        return str.toString();
+    }
+    @Override
+    public String toString() {
+        // TODO Auto-generated method stub
+        return "CommandEnquanto [condition=" + condition + ", lista=" + lista + "]";
+    }
 
 }
+
